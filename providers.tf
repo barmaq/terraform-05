511a562132d51f1fd436ee8b17c389e7fc68d66c
Шумский Виктор <victorsh@electro.local>
2024-11-20 16:48:20 +0300

    added backend block to providers - terraform-05

--- providers.tf
+++ providers.tf
@@ -5,6 +5,23 @@ terraform {
     }
   }
   required_version = "~>1.8.4"
+
+# отправка state
+    backend "s3" {
+    endpoints = { s3 = "https://storage.yandexcloud.net"    }
+    bucket = "barmaq-tfstate"
+    region = "ru-central1"
+    key    = "terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # необходимая опция при описании бэкенда для Terraform версии 1.6.1 и старше.
+    skip_s3_checksum            = true # необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
+
+
+
 }
 
 provider "yandex" {
--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "default_zone" {
 }
 variable "default_cidr" {
   type        = list(string)
-#  default     = ["10.0.1.0/24"]
+  default     = ["10.0.1.0/24"]
   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
 }
 
