f700882a0406ddea090bf1d2dad3beb7c357ba44
Victor Shumskiy <krishna2003@gmail.com>
2024-11-21 17:26:04 +0300

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -15,20 +15,20 @@ variable "default_zone" {
   default     = "ru-central1-a"
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
-variable "default_cidr" {
-  type        = list(string)
-  default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
-}
+# variable "default_cidr" {
+#   type        = list(string)
+#   default     = ["10.0.1.0/24"]
+#   description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+# }
 
-variable "vpc_name" {
-  type        = string
-  default     = "develop"
-  description = "VPC network&subnet name"
-}
+# variable "vpc_name" {
+#   type        = string
+#   default     = "develop"
+#   description = "VPC network&subnet name"
+# }
 
 
-###example vm_web var
+##example vm_web var
 variable "vm_name" {
   type        = string
   default     = "netology-vm"
@@ -49,18 +49,18 @@ locals {
 
 
 #для задания 4
-variable "vpc_prod" {
-  description = "vpc_prod список"
-  type = list(object({
-    zone = string
-    cidr = string
-  }))
-  default = [
-    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
-    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
-    { zone = "ru-central1-d", cidr = "10.0.3.0/24" }
-  ]
-}
+# variable "vpc_prod" {
+#   description = "vpc_prod список"
+#   type = list(object({
+#     zone = string
+#     cidr = string
+#   }))
+#   default = [
+#     { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+#     { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+#     { zone = "ru-central1-d", cidr = "10.0.3.0/24" }
+#   ]
+# }
 
 variable "vpc_dev" {
   description = "vpc_dev список"
